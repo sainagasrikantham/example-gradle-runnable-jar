94f9ffe20424e48555c121669899e5798b5fe657
Sainaga Srikantham <sainagas@zillowgroup.com>
2020-12-07 21:06:05 -0500

    Added sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Sample test running...");
+        assertTrue(true);
+    }
+}
